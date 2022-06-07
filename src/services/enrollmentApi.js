eae219ee2d3258000150a4b5c844464204703d2a
Leonardo Otero <leonardo.otero390@gmail.com>
2022-06-07 19:22:02 -0400

    first commit

---
+++ src/services/enrollmentApi.js
@@ -0,0 +1,27 @@
+import api from './api';
+
+export async function save(body, token) {
+  const response = await api.post('/enrollments', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function getPersonalInformations(token) {
+  try {
+    const response = await api.get('/enrollments', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
+    return response.data;
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+//
