4262b7dd7ddf8c13211e2c41ec8878b10a4d3d5b
SilasFr <silas23.fr@gmail.com>
2022-06-08 17:01:13 -0300

    feat: render rooms and availability

--- src/components/Dashboard/Hotel/RoomSelection.js
+++ src/components/Dashboard/Hotel/RoomSelection.js
@@ -1,5 +1,8 @@
 import { Box, IconButton, List, ListItem, ListItemText, Typography } from '@mui/material';
 import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import styled from 'styled-components';
+import { useState } from 'react';
 
 const styles = {
   list: {
@@ -11,28 +14,48 @@ const styles = {
 };
 
 export default function RoomSelection() {
+  const [ocupiedRoom, setOccupiedRoom] = useState(false);
+
+  function renderRoomCapacity(room) {
+    const capacity = [...new Array(room.capacity)].map((value, index) => index + 1 > room.occupation);
+    return capacity;
+  }
   return (
     <Box>
       <Typography variant="h6" color="textSecondary">
         Ótima pedida! Agora escolha seu quarto:
       </Typography>
-      <List sx={styles.list}>
+      <Box
+        sx={{
+          height: '220px',
+          width: '100%',
+          mt: 4,
+
+          display: 'flex',
+          gap: '5px',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+
+          overflowX: 'scroll',
+        }}
+      >
         {roomsMock.map((room) => {
+          const availability = renderRoomCapacity(room);
+          console.log(availability);
           return (
-            <ListItem
-              key={room.id}
-              disabled={room.capacity === room.occupation}
-              secondaryAction={[...new Array(room.capacity)].map(() => (
-                <IconButton key={Math.random()} edge="end" aria-label="room-availability">
-                  <PersonOutlineRoundedIcon key={Math.random()} />
-                </IconButton>
-              ))}
-            >
-              <ListItemText primary={room.number} />
-            </ListItem>
+            <StyledRoomButton key={Math.random()} disabled={room.capacity === room.occupation}>
+              <Typography>{room.number}</Typography>
+              <Box>
+                {availability.map((el) => (
+                  <IconButton key={Math.random()} edge="end" aria-label="room-availability">
+                    {el ? <PersonOutlineRoundedIcon key={Math.random()} /> : <PersonRoundedIcon />}
+                  </IconButton>
+                ))}
+              </Box>
+            </StyledRoomButton>
           );
         })}
-      </List>
+      </Box>
     </Box>
   );
 }
@@ -43,4 +66,47 @@ const roomsMock = [
   { id: 3, number: '103', capacity: 2, occupation: 2 },
   { id: 4, number: '104', capacity: 1, occupation: 0 },
   { id: 5, number: '105', capacity: 3, occupation: 1 },
+  { id: 1, number: '101', capacity: 3, occupation: 0 },
+  { id: 2, number: '102', capacity: 2, occupation: 1 },
+  { id: 3, number: '103', capacity: 2, occupation: 2 },
+  { id: 4, number: '104', capacity: 1, occupation: 0 },
+  { id: 5, number: '105', capacity: 3, occupation: 1 },
+  { id: 1, number: '101', capacity: 3, occupation: 0 },
+  { id: 2, number: '102', capacity: 2, occupation: 1 },
+  { id: 3, number: '103', capacity: 2, occupation: 2 },
+  { id: 4, number: '104', capacity: 1, occupation: 0 },
+  { id: 5, number: '105', capacity: 3, occupation: 1 },
+  { id: 1, number: '101', capacity: 3, occupation: 0 },
+  { id: 2, number: '102', capacity: 2, occupation: 1 },
+  { id: 3, number: '103', capacity: 2, occupation: 2 },
+  { id: 4, number: '104', capacity: 1, occupation: 0 },
+  { id: 5, number: '105', capacity: 3, occupation: 1 },
+  { id: 1, number: '101', capacity: 3, occupation: 0 },
+  { id: 2, number: '102', capacity: 2, occupation: 1 },
+  { id: 3, number: '103', capacity: 2, occupation: 2 },
+  { id: 4, number: '104', capacity: 1, occupation: 0 },
+  { id: 5, number: '105', capacity: 3, occupation: 1 },
+  { id: 1, number: '101', capacity: 3, occupation: 0 },
+  { id: 2, number: '102', capacity: 2, occupation: 1 },
+  { id: 3, number: '103', capacity: 2, occupation: 2 },
+  { id: 4, number: '104', capacity: 1, occupation: 0 },
+  { id: 5, number: '105', capacity: 3, occupation: 1 },
+  { id: 1, number: '101', capacity: 3, occupation: 0 },
+  { id: 2, number: '102', capacity: 2, occupation: 1 },
 ];
+
+const StyledRoomButton = styled.button`
+  width: 190px;
+  border: 1px solid #cecece;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 12px 0 16px;
+
+  background-color: #fff;
+
+  &:disabled {
+    background-color: #cecece;
+  }
+`;
