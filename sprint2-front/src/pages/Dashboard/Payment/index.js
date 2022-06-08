b5da39e89ff5bf5523d71fc96f446028c3c0b9e8
SilasFr <silas23.fr@gmail.com>
2022-06-08 11:08:30 -0300

    first commit

---
+++ sprint2-front/src/pages/Dashboard/Payment/index.js
@@ -0,0 +1,5 @@
+import PaymentPage from '../../../components/Dashboard/Payment';
+
+export default function Payment() {
+  return <PaymentPage />;
+}
---
+++ sprint2-front/src/services/eventApi.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+import api from './api';
+
+export async function getEventInfo() {
+  const response = await api.get('/event');
+  return response.data;
+}
+//
