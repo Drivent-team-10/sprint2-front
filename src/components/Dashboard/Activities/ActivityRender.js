4fa328e4513e08e394eb85fd8f7c132b8b37de1c
SilasFr <silas23.fr@gmail.com>
2022-06-15 16:31:15 -0300

    feat: filter activity by day

--- src/components/Dashboard/Activities/ActivityRender.js
+++ src/components/Dashboard/Activities/ActivityRender.js
@@ -1,16 +1,75 @@
-import { Box, Typography } from '@material-ui/core';
-import { useState } from 'react';
+import { Box, Container, Typography } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import Button from '../../Form/Button';
+import useToken from '../../../hooks/useToken';
+import { getActivitiesByEventId } from '../../../services/activityApi';
+import dayjs from 'dayjs';
+import updateLocale from 'dayjs/plugin/updateLocale';
 
-const daysMock = ['Sexta, 22/10', 'Sábado, 23/10', 'Domingo, 24/10'];
+dayjs.extend(updateLocale);
+
+dayjs.updateLocale('en', {
+  weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+});
 
 export default function Activities() {
+  const token = useToken();
   const [props, setProps] = useState({});
+  const [daysOfEvent, setDaysOfEvent] = useState([]);
+  const [activities, setActivities] = useState(null);
+  const [auditoriums, setAuditoriums] = useState([]);
+  const [dayFilter, setDayFilter] = useState(null);
 
-  function handleSelection(i) {
+  useEffect(() => {
+    async function loadActivities() {
+      const response = await getActivitiesByEventId(1, token);
+      setActivities(response);
+    }
+    try {
+      loadActivities();
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!activities) return;
+    const result = [];
+
+    for (let activity of activities) {
+      if (result.includes(activity.auditorium.name)) continue;
+      result.push(activity.auditorium.name);
+    }
+    setAuditoriums(result);
+    setDaysOfEvent(filterDaysOfEvent());
+  }, [activities]);
+
+  function handleSelection(i, day) {
+    if (day === dayFilter) setDayFilter(null);
     if (props[i]) return setProps({});
     const selectionConfig = { backgroundColor: '#FFD37D' };
     setProps({ [i]: selectionConfig });
+
+    setDayFilter(day);
+  }
+
+  if (!activities || !auditoriums) {
+    return 'Carregando...';
+  }
+
+  function filterDaysOfEvent() {
+    const result = [];
+
+    for (let activity of activities) {
+      const day = formatDayDisplay(activity.startsAt);
+
+      if (!result.includes(day)) result.push(day);
+    }
+    return result;
+  }
+
+  function formatDayDisplay(datetime) {
+    return dayjs(datetime).format('dddd, DD/MM');
   }
 
   return (
@@ -22,14 +81,36 @@ export default function Activities() {
             Primeiro, filtre pelo dia do evento:
           </Typography>
         )}
-        <Box sx={{ display: 'flex', gap: '17px' }}>
-          {daysMock.map((day, i) => (
-            <Button key={i} sx={props[i]} onClick={(e) => handleSelection(i)}>
+        <Box sx={styles.container}>
+          {daysOfEvent.map((day, i) => (
+            <Button key={`${i}${day}`} sx={props[i]} onClick={(e) => handleSelection(i, day)}>
               {day}
             </Button>
           ))}
         </Box>
+        {dayFilter && (
+          <Box sx={styles.container}>
+            {auditoriums.map((auditorium, i) => (
+              <Box sx={styles.activityContainer}>
+                <Typography key={`${i}${auditorium}`}> {auditorium} </Typography>
+                <Container key={i}>
+                  {activities.map((activity) => {
+                    const isActivityFromDayFiltered = formatDayDisplay(activity.startsAt) === dayFilter;
+                    if (activity.auditorium.name === auditorium && isActivityFromDayFiltered) {
+                      return <Button> {activity.name} </Button>;
+                    }
+                  })}
+                </Container>
+              </Box>
+            ))}
+          </Box>
+        )}
       </Box>
     </Box>
   );
 }
+
+const styles = {
+  container: { display: 'flex', gap: '17px' },
+  activityContainer: { display: 'flex', flexDirection: 'column' },
+};
