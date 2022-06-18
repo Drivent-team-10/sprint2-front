21be07f858ed15da989a24ebebaccd3973e17029
Leonardo Otero <leonardo.otero390@gmail.com>
2022-06-18 13:45:59 -0400

    feat: warn when payment isn't done

--- src/components/Dashboard/Activities/index.js
+++ src/components/Dashboard/Activities/index.js
@@ -1,5 +1,27 @@
+import usePaymentData from '../../../hooks/api/usePayment';
+import { Box } from '@material-ui/core';
+import { StyledTypography } from '../style';
 import Activities from './ActivityRender';
 
-export default function ActivitiesPage() {
-  return <Activities />;
+export default function HotelPage() {
+  const { payment } = usePaymentData();
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+      {
+        <>
+          {!payment?.length ? (
+            <Box display="flex" alignItems="center" justifyContent="center" height="80%">
+              <StyledTypography variant="h6" color="textSecondary">
+                Você precisa ter confirmado pagamento antes de fazer a escolha de atividades
+              </StyledTypography>
+            </Box>
+          ) : (
+            <Activities />
+          )}
+        </>
+      }
+    </>
+  );
 }
