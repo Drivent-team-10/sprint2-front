6edaf2be9b9c704d162de00069a34cb29860f475
Leonardo Otero <leonardo.otero390@gmail.com>
2022-06-18 13:55:45 -0400

    feat: warn when reservation type == online

--- src/components/Dashboard/Activities/index.js
+++ src/components/Dashboard/Activities/index.js
@@ -5,7 +5,7 @@ import Activities from './ActivityRender';
 
 export default function HotelPage() {
   const { payment } = usePaymentData();
-
+  if (payment) console.log(payment[0].reservation.type);
   return (
     <>
       <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
@@ -15,6 +15,13 @@ export default function HotelPage() {
             <Box display="flex" alignItems="center" justifyContent="center" height="80%">
               <StyledTypography variant="h6" color="textSecondary">
                 Você precisa ter confirmado pagamento antes de fazer a escolha de atividades
+              </StyledTypography>
+            </Box>
+          ) : payment[0].reservation.type === 'online' ? (
+            <Box display="flex" alignItems="center" justifyContent="center" height="80%">
+              <StyledTypography variant="h6" color="textSecondary">
+                Sua modalidade de ingresso não necessita escolher atividade.
+                <br /> Você terá acesso a todas as atividades.
               </StyledTypography>
             </Box>
           ) : (
